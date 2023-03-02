0baa1f1ad18b5dbb54283599ae058821db1941a9
Ikhwanwp <wiratama900@gmail.com>
2023-03-02 21:52:07 +0700

    first

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="w-[436px] h-[104px] bg-[#37ADB5] rounded-full blur-[300px] fixed top-[394px] left-[270px]"></div>
+      <div className="w-[169px] h-[413px] bg-[#87515B] rounded-full blur-[300px] fixed top-[438px] left-[642px] -rotate-[53deg]"></div>
+      <div className="mt-[1000px]">
+        <h1 className="text-white">
+          Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint
+          cillum sint consectetur cupidatat. Lorem ipsum dolor sit amet, officia
+          excepteur ex fugiat reprehenderit enim labore culpa sint ad nisi Lorem
+          pariatur mollit ex esse exercitation amet. Nisi anim cupidatat
+          excepteur officia. Reprehenderit nostrud nostrud ipsum Lorem est
+          aliquip amet voluptate voluptate dolor minim nulla est proident.
+          Nostrud officia pariatur ut officia. Sit irure elit esse ea nulla sunt
+          ex occaecat reprehenderit commodo officia dolor Lorem duis laboris
+          cupidatat officia voluptate. Culpa proident adipisicing id nulla nisi
+          laboris ex in Lorem sunt duis officia eiusmod. Aliqua reprehenderit
+          commodo ex non excepteur duis sunt velit enim. Voluptate laboris sint
+          cupidatat ullamco ut ea consectetur et est culpa et culpa duis.
+        </h1>
+      </div>
     </div>
   );
 }
---
+++ src/components/KonsolePorto.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+function KonsolePorto() {
+  return(
+    
+  );
+}
+
+export default KonsolPorto;
